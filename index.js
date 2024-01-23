18a7e787582c0cf6b7f7d29f7fedfb16c3ca101e
Brian Fernandez <bfernandez@cetep.cl>
2024-01-23 13:55:06 -0300

    Add server.js, .gitignore, package.json, and index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+
+function openWebPage() {
+  return puppeteer.launch().then(async browser => {
+    const page = await browser.newPage();
+    await page.goto('https://www.doctoralia.cl/buscar?q=Psiquiatra&loc=Santiago&filters%5Bspecializations%5D%5B%5D=55');
+    await page.click('button[id="onetrust-accept-btn-handler"]')
+    await page.screenshot({path: 'example.png'});
+    await browser.close();
+  });
+}
+
+openWebPage();
